57c70099f90889f728bc820f45550309853fb2e0
Mayron <mjr_4077@hotmail.co.uk>
2020-11-04 16:26:43 +0000

    Small tweak

--- src/components/home/ready-to-get-started.tsx
+++ src/components/home/ready-to-get-started.tsx
@@ -13,21 +13,24 @@ const ReadyToGetStarted: React.FC = () => {
         text-align: center;
         padding-top: 60px !important;
 
+        p {
+          font-size: ${vars.largeFontSize};
+          padding-top: 10px;
+          padding-bottom: 40px;
+        }
+
         ${media.down("xs")`
           padding-bottom: 60px;
+          
+          p {
+            padding-left: 40px;
+            padding-right: 40px;
+          }
         `};
       `}
     >
       <h3>Ready to get started?</h3>
-      <p
-        css={css`
-          font-size: ${vars.largeFontSize};
-          padding-top: 10px;
-          padding-bottom: 40px;
-        `}
-      >
-        Visit the MayronUI download page to learn more.
-      </p>
+      <p>Visit the MayronUI download page to learn more.</p>
       <Button type="secondary" size="lg" href="/p/mayronui">
         Go to Download Page
       </Button>
